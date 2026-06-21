9aaaa8e201abf98c504c5baec5edb0b687e0dcd7
negima <lkys5379@gmail.com>
2026-06-21 17:33:48 +0900

    modelファイル作成

---
+++ app/Models/Income.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Income extends Model
+{
+    use HasFactory;
+
+    //
+
+    protected $fillable = [
+        'user_id',
+        'income_date',
+        'memo',
+        'expected_income',
+        'amount',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
