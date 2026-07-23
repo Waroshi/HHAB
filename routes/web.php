170a8a2555c0cde74f274695e83eef323f771ec3
taka4635 <162914232+taka4635@users.noreply.github.com>
2026-07-23 13:03:27 +0900

    バックエンド変更削除

--- routes/web.php
+++ routes/web.php
@@ -21,33 +21,13 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
+
 Route::get('/transaction', [TransactionController::class, 'index'])->name('transactions.index');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-
-    Route::get('/scan', function () {
-        return Inertia::render('Scan');
-    })->name('readings.index');
-
-    Route::get('/scan/result', function () {
-        return Inertia::render('ReadingResult');
-    })->name('readings.result');
-
-    Route::get('/calendar', function () {
-        return Inertia::render('Calendar');
-    })->name('calendar.index');
-
-    Route::get('/reports', function () {
-        return Inertia::render('Report');
-    })->name('reports.index');
-
-    Route::get('/menu', function () {
-        return Inertia::render('Menu');
-    })->name('menu.index');
-
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
